9c862124564947709696032c355cd0ff069f8114
chenkun1998@163.com <chenkun1998@163.com>
2019-01-29 23:45:00 -0500

    update

--- django-sso/settings.py
+++ django-sso/settings.py
@@ -95,7 +95,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djcelery',
     'corsheaders',
     'rest_framework',
     'rest_framework_swagger',
